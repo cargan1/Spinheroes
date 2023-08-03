7364c02188932017892091bc4eebd615d14900ea
Daniel Cargan <daniel@3wm.dkl>
2023-08-03 16:05:18 +0200

    Rating plugin + changes

--- src/js/jquery.star-rating-svg.js
+++ src/js/jquery.star-rating-svg.js
@@ -1,182 +1,3 @@
-'use strict';
-
-const hamburger = document.querySelector(".hamburger");
-const drawer = document.querySelector(".mobile-drawer");
-
-hamburger.addEventListener("click", () => {
-  //Hamburger Animation
-  hamburger.classList.toggle("toggle");
-  drawer.classList.toggle("open");
-  document.body.classList.toggle("overflow-hidden");
-});
-
-const flagCta = document.querySelector(".flag-mobile");
-const flagDropdown = document.querySelector(".location-dropdown");
-flagCta.addEventListener("click", () => {
-  //Hamburger Animation
-  flagDropdown.classList.toggle("hidden");
-});
-
-const popCta = document.querySelector("#popup-cta");
-const popup = document.querySelector(".popup-section");
-const popClose = document.querySelector(".pop-close-icon");
-
-popCta.addEventListener("click", () => {
-  //Hamburger Animation
-  popup.classList.toggle("hidden");
-});
-
-popClose.addEventListener("click", () => {
-  popup.classList.add("hidden");
-});
-
-let scrollpos = window.scrollY;
-
-const scrollDown = document.querySelector(".scroll-down");
-const scrollChange = 1;
-
-const add_class_on_scroll = () => scrollDown.classList.add("hidden");
-const remove_class_on_scroll = () => scrollDown.classList.remove("hidden");
-
-window.addEventListener("scroll", function () {
-  scrollpos = window.scrollY;
-
-  if (scrollpos >= scrollChange) {
-    add_class_on_scroll();
-  } else {
-    remove_class_on_scroll();
-  }
-});
-
-let voteAmounts = document.querySelectorAll(".voteAmount");
-
-voteAmounts.forEach((el) => {
-  let voteNumber = randomIntFromInterval(231, 891);
-  el.textContent = voteNumber;
-});
-
-function randomIntFromInterval(min, max) {
-  // min and max included
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
-// $(".star-icon").each(function () {
-//   let numberCorrect = parseInt(
-//     $(this)
-//       .closest(".rating-container")
-//       .siblings(".votes")
-//       .find(".voteAmount")
-//       .text()
-//   );
-//   let newVote = (numberCorrect += 1);
-//   $(this)
-//     .hover(
-//       function () {
-//         $(this).prevAll().addBack().css("color", "#ffc300");
-//       },
-//       function () {
-//         if (!$(this).parent().attr("data-rating")) {
-//           $(this).prevAll().addBack().css("color", "#49487b");
-//         } else {
-//           $(this)
-//             .siblings()
-//             .addBack()
-//             .each(function (index) {
-//               index + 1 <= $(this).parent().attr("data-rating")
-//                 ? $(this).css("color", "#ffc300")
-//                 : $(this).css("color", "#49487b");
-//             });
-//         }
-//       }
-//     )
-//     .click(function () {
-//       $(this)
-//         .parent()
-//         .attr("data-rating", $(this).prevAll().length + 1);
-
-//       $(this)
-//         .closest(".rating-container")
-//         .siblings(".votes")
-//         .find(".voteAmount")
-//         .text(newVote);
-//     });
-// });
-
-// $(".rating-star").each(function () {
-//   let numberCorrect = parseInt(
-//     $(this)
-//       .closest(".rating-container")
-//       .siblings(".votes")
-//       .find(".voteAmount")
-//       .text()
-//   );
-//   let newVote = (numberCorrect += 1);
-//   $(this)
-//     .hover(
-//       function () {
-//         $(this).addBack().toggleClass("activo");
-//       },
-//       function () {
-//         $(this).prevAll().addBack().addClass("fullStar");
-//       },
-//       function () {
-//         $(this).nextAll().addBack().addClass("noStar");
-//       }
-//       // function () {
-//       //   if (!$(this).parent().attr("data-rating")) {
-//       //     console.log("Nothing");
-//       //     $(this).prevAll().addBack().addClass("fullStar");
-//       //   } else {
-//       //     console.log("Tested");
-//       //     $(this)
-//       //       .siblings()
-//       //       .addBack()
-//       //       .each(function (index) {
-//       //         index <= $(this).parent().attr("data-rating")
-//       //           ? $(this).addClass("fullStar")
-//       //           : $(this).addClass("noStar");
-//       //       });
-//       //   }
-//       // }
-//     )
-//     .click(function () {
-//       $(this)
-//         .parent()
-//         .attr("data-rating", $(this).prevAll().length + 1);
-
-//       $(this)
-//         .closest(".rating-container")
-//         .siblings(".votes")
-//         .find(".voteAmount")
-//         .text(newVote);
-//     });
-//   $(this)
-//     .children()
-//     .hover(function () {
-//       $(this).parent().toggleClass("halfStar");
-//     });
-// });
-
-$(".rato-container").each(function () {
-  let numberCorrect = parseInt(
-    $(this)
-      .closest(".rato-container")
-      .siblings(".votes")
-      .find(".voteAmount")
-      .text()
-  );
-  let newVote = (numberCorrect += 1);
-  $(this).click(function () {
-    $(this)
-      .closest(".rato-container")
-      .siblings(".votes")
-      .find(".voteAmount")
-      .text(newVote);
-  });
-});
-
-// basic use comes with defaults values
-
 /*
  *  jQuery StarRatingSvg v1.2.0
  *
@@ -186,7 +7,8 @@ $(".rato-container").each(function () {
  *  Licensed under MIT
  */
 
-(function ($, window, document, undefined$1) {
+(function ($, window, document, undefined) {
+  "use strict";
 
   // Create the defaults once
   var pluginName = "starRating";
@@ -198,7 +20,7 @@ $(".rato-container").each(function () {
     emptyColor: "lightgray",
     hoverColor: "orange",
     activeColor: "gold",
-    ratedColor: "#d19900",
+    ratedColor: "crimson",
     useGradient: true,
     readOnly: false,
     disableAfterRate: true,
@@ -616,17 +438,4 @@ $(".rato-container").each(function () {
       }
     });
   };
-})(jQuery);
-
-$(".my-rating").each(function () {
-  $(this).starRating({
-    initialRating: 5.0,
-    starSize: 20,
-    emptyColor: "#5c5b9e",
-    hoverColor: "#e9b629",
-    activeColor: "#e9b629",
-    ratedColor: "blue",
-    strokeWidth: 0,
-    useGradient: false,
-  });
-});
+})(jQuery, window, document);
