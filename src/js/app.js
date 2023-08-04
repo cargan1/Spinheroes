ab464edd68e6841aa9abf4f44693467b6c48e891
Daniel Cargan <daniel@3wm.dkl>
2023-08-04 12:08:34 +0200

    changed ratings

--- src/js/app.js
+++ src/js/app.js
@@ -57,137 +57,41 @@ function randomIntFromInterval(min, max) {
   // min and max included
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
+$(document).ready(function () {
+  $(".rating-block").each(function () {
+    let elid = "el-" + Math.random().toString(16).slice(2);
+    $(this).attr("id", elid);
 
-// $(".star-icon").each(function () {
-//   let numberCorrect = parseInt(
-//     $(this)
-//       .closest(".rating-container")
-//       .siblings(".votes")
-//       .find(".voteAmount")
-//       .text()
-//   );
-//   let newVote = (numberCorrect += 1);
-//   $(this)
-//     .hover(
-//       function () {
-//         $(this).prevAll().addBack().css("color", "#ffc300");
-//       },
-//       function () {
-//         if (!$(this).parent().attr("data-rating")) {
-//           $(this).prevAll().addBack().css("color", "#49487b");
-//         } else {
-//           $(this)
-//             .siblings()
-//             .addBack()
-//             .each(function (index) {
-//               index + 1 <= $(this).parent().attr("data-rating")
-//                 ? $(this).css("color", "#ffc300")
-//                 : $(this).css("color", "#49487b");
-//             });
-//         }
-//       }
-//     )
-//     .click(function () {
-//       $(this)
-//         .parent()
-//         .attr("data-rating", $(this).prevAll().length + 1);
-
-//       $(this)
-//         .closest(".rating-container")
-//         .siblings(".votes")
-//         .find(".voteAmount")
-//         .text(newVote);
-//     });
-// });
-
-// $(".rating-star").each(function () {
-//   let numberCorrect = parseInt(
-//     $(this)
-//       .closest(".rating-container")
-//       .siblings(".votes")
-//       .find(".voteAmount")
-//       .text()
-//   );
-//   let newVote = (numberCorrect += 1);
-//   $(this)
-//     .hover(
-//       function () {
-//         $(this).addBack().toggleClass("activo");
-//       },
-//       function () {
-//         $(this).prevAll().addBack().addClass("fullStar");
-//       },
-//       function () {
-//         $(this).nextAll().addBack().addClass("noStar");
-//       }
-//       // function () {
-//       //   if (!$(this).parent().attr("data-rating")) {
-//       //     console.log("Nothing");
-//       //     $(this).prevAll().addBack().addClass("fullStar");
-//       //   } else {
-//       //     console.log("Tested");
-//       //     $(this)
-//       //       .siblings()
-//       //       .addBack()
-//       //       .each(function (index) {
-//       //         index <= $(this).parent().attr("data-rating")
-//       //           ? $(this).addClass("fullStar")
-//       //           : $(this).addClass("noStar");
-//       //       });
-//       //   }
-//       // }
-//     )
-//     .click(function () {
-//       $(this)
-//         .parent()
-//         .attr("data-rating", $(this).prevAll().length + 1);
-
-//       $(this)
-//         .closest(".rating-container")
-//         .siblings(".votes")
-//         .find(".voteAmount")
-//         .text(newVote);
-//     });
-//   $(this)
-//     .children()
-//     .hover(function () {
-//       $(this).parent().toggleClass("halfStar");
-//     });
-// });
-
-$(".rating-block").each(function () {
-  let elid = "el-" + Math.random().toString(16).slice(2);
-  $(this).attr("id", elid);
-
-  let numberCorrect = parseInt(
-    $(this)
-      .closest(".offer-item--ratings")
-      .children(".votes")
-      .find(".voteAmount")
-      .text()
-  );
-  let newVote = (numberCorrect += 1);
-
-  $(this)
-    .children()
-    .each(function () {
-      let newid;
-      if ($(this).attr("id")) {
-        newid = elid + "-" + $(this).attr("id");
-        $(this).attr("id", newid);
-        $(this).attr("name", newid);
-      } else {
-        newid = elid + "-" + $(this).attr("for");
-        $(this).attr("for", newid);
-      }
-    });
-  $(this)
-    .children()
-    .click(function () {
+    let numberCorrect = parseInt(
       $(this)
         .closest(".offer-item--ratings")
         .children(".votes")
         .find(".voteAmount")
-        .text(newVote);
-    });
+        .text()
+    );
+    let newVote = (numberCorrect += 1);
+
+    $(this)
+      .children()
+      .each(function () {
+        let newid;
+        if ($(this).attr("id")) {
+          newid = elid + "-" + $(this).attr("id");
+          $(this).attr("id", newid);
+          $(this).attr("name", newid);
+        } else {
+          newid = elid + "-" + $(this).attr("for");
+          $(this).attr("for", newid);
+        }
+      });
+    $(this)
+      .children()
+      .click(function () {
+        $(this)
+          .closest(".offer-item--ratings")
+          .children(".votes")
+          .find(".voteAmount")
+          .text(newVote);
+      });
+  });
 });
